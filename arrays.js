79d8bfa47c64e79cd459e426bb062f0df854a252
asmaabelhadjJS <asmaa1belhadj@gmail.com>
2024-11-20 12:23:04 +0100

    ex 2

--- arrays.js
+++ arrays.js
@@ -1,3 +1,30 @@
 //ex 1
 let number =[1,2,3,4,5];
 console.log(number);
+
+ //ex 2 
+let elements =["apple", "banana", "cherry"];
+console.log(elements[1]);
+
+
+
+//ex 3
+let nombre =[10, 20, 30, 40];
+console.log(nombre);
+nombre[2]= 50;
+console.log(nombre);
+
+//ex 4
+let fruits =["mango", "peach", "pear"];
+console.log(fruits);
+fruits[3]="orange";
+console.log(fruits);
+
+
+//ex 5
+let élèments=["dog", "cat", "bird"];
+console.log(élèments);
+élèments.pop();
+console.log(élèments);
+
+
